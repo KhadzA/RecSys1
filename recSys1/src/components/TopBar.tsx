433a751011c97214a20ab6d714427d6d096d87d1
KhadzA <alimkhadz111@gmail.com>
2026-03-10 19:21:48 +0800

    Logo

--- recSys1/src/components/TopBar.tsx
+++ recSys1/src/components/TopBar.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Sun, Moon } from "lucide-react";
-import logo from "./assets/Logo.svg";
+import logo from "../assets/Logo.svg";
 
 interface TopBarProps {
   dark: boolean;
